ff9834db77c39db76a871f11086162fabaa59c32
Angelo <locktohang@gmail.com>
2019-09-10 15:01:13 +0300

    post migration

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::get();
+        $posts = Post::published()->get();
        // dd($posts);
         return view('posts.index', compact('posts'));
     }
@@ -49,7 +49,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::published()->where('id', $id)->firstOrFail();
+        return $post;
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'slug', 'content', 'online'];  
+
+    public function scopePublished($query){
+        return $query->where('online', true)->whereRaw('created_at < NOW()');
+    }
+
+    public function getSlugAttribute($value){
+        if(empty($value)){
+            $this->attributes['slug'] = Str::slug($this->title);
+        }
+    }
+
+    public function getDates(){
+        return ['create_at', 'updated_at'];
+    }
 }
